51d9d68f367e2002de33549790d15cca7b47f711
Pushpak <36948404+PushpakAg@users.noreply.github.com>
2024-05-17 13:29:20 +0530

    Update depthmap.py

--- depthmap.py
+++ depthmap.py
@@ -4,9 +4,9 @@ import time
 import numpy as np
 from collections import deque
 
-# model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
-# model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
-model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+# model_type = "DPT_Large"     # MiDaS v3 - Large 
+# model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid   
+model_type = "MiDaS_small"  # MiDaS v2.1 - Small  
 
 midas = torch.hub.load("intel-isl/MiDaS", model_type)
 
